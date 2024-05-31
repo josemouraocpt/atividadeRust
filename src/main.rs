ce22d9d535ca8bed98f15667601789f421ac38c1
josemouraocpt <josemouraocpt@gmail.com>
2024-05-30 22:51:59 -0300

    ações do usuário

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::{self};
 use std::fs::File;
 use std::io::{BufReader, stdin};
 use std::path::Path;
@@ -73,7 +74,7 @@ impl HashMap{
                 println!("Nome do hotel: {}",self.values[mid].reserva.nome_hotel);
                 println!("Número do quarto: {}",self.values[mid].reserva.numero_quarto);
                 println!("Data do check-in: {}",self.values[mid].reserva.data_check_in);
-                println!("Data do chech-out: {}",self.values[mid].reserva.data_check_out);
+                println!("Data do checK-out: {}\n",self.values[mid].reserva.data_check_out);
                 return;
             } else if self.values[mid] < self.values[target]{
                 low = mid + 1
@@ -81,14 +82,14 @@ impl HashMap{
                 high = mid - 1
             }
         }
-        println!("Reserva não encontrada!");
+        println!("Reserva não encontrada!\n");
         return;
     }
 
     fn remove(&mut self, target: String){
         //tratar o valor caso seja menor que 10
         let index: usize = target[..2].parse().unwrap();
-        println!("Reserva removida!");
+        println!("Reserva removida!\n");
         self.values.remove(index);
         return;
     }
@@ -125,28 +126,91 @@ fn main() {
     let reader = BufReader::new(file);
     let json: Vec<Reserva> = serde_json::from_reader(reader).unwrap();
     let mut hash_map: HashMap = HashMap::new();
-    let mut input: String = String::new();
-    let mut input2: String = String::new();
     for i in 0..=json.len()-1{
         let reserva: Reserva = Reserva::new(json[i].numero_reserva, json[i].nome_hotel.to_owned(), json[i].numero_quarto, json[i].data_check_in.to_owned(), json[i].data_check_out.to_owned());
         let hash: Hash = Hash::new(reserva, hash_map.values.len() as i32);
         hash_map.push(hash);
     }
-    //Criar um loop até o usuário decidir parar
-    //opções de incluir uma nova reserva, remover uma reserva e buscar uma reserva, cancelar uma reserva e realizar um check-out
-    println!("Qual reserva você deseja ver mais informações:\n");
-    hash_map.display();
-    println!("\nEscolha entre as opções 0 a {}: ", hash_map.values.len()-1);
+    println!("Bem-vindo ao sistema de reserva de hotéis.\n");
+    loop{
+        let mut option: String = String::new();
+        let mut num_reserva: String = String::new();
+        let mut nm_re: String = String::new();
+        let mut nm_ht: String = String::new();
+        let mut nm_qt: String = String::new();
+        let mut data_in: String = String::new();
+        let mut data_ou: String = String::new();
 
-    stdin().read_line(&mut input).unwrap();
-    let mut res: usize = input.trim().parse().unwrap();
-    hash_map.get(res);
+        print!("Para ver informações sobre as reservas pressione insira: RESERVAS\n");
+        print!("Para incluir uma nova reserva insira: NOVA\n");
+        print!("Para cancelar uma reserva insira: CANCELAR\n");
+        print!("Para remover uma reserva insira: REMOVER\n");
+        print!("Para realizar o check-out de uma reserva insira: CHECK-OUT\n");
+        print!("Para sair da aplicação insira: SAIR\n");
+        io::stdin().read_line(&mut option).unwrap();
 
-    println!("\nQual reserva você deseja remover: ");
-    println!("\nEscolha entre as opções 0 a {}: ", hash_map.values.len()-1);
+        match option.trim(){
+            "SAIR" | "sair" | "Sair" => break,
+            "RESERVAS" | "reservas" | "Reservas" => {
+                println!("Qual reserva você deseja ver mais informações:\n");
+                hash_map.display();
+                println!("\nEscolha entre as opções 0 a {}: ", hash_map.values.len()-1);
+                io::stdin().read_line(&mut num_reserva).expect("Insira um valor númerico válido");
+                let res: i32 = num_reserva.trim().parse().unwrap();
+                if res > (hash_map.values.len()-1) as i32 || res < 0{
+                    println!("Valor inserido não é válido!");
+                    continue;
+                } else{
+                    let target: usize = num_reserva.trim().parse().unwrap();
+                    hash_map.get(target);
+                }
+            },
+            "CANCELAR" | "REMOVER"| "CHECK-OUT" | "cancelar" | "Cancelar" | "remover" | "Remover" | "check-out" | "Check-out" => {
+                println!("Qual reserva você deseja {}:\n", option.to_lowercase());
+                hash_map.display();
+                println!("\nEscolha entre as opções 0 a {}: ", hash_map.values.len()-1);
+                io::stdin().read_line(&mut num_reserva).expect("Insira um valor númerico válido");
+                let res: i32 = num_reserva.trim().parse().unwrap();
+                if res > (hash_map.values.len()-1) as i32 || res < 0{
+                    println!("Valor inserido não é válido!");
+                    continue;
+                } else{
+                    if res < 10{
+                        let res_str = "0".to_owned() + &res.to_string();
+                        hash_map.remove(res_str);
+                    }else {
+                        hash_map.remove(res.to_string());
+                    }
+                }
+            },
+            "NOVA" | "nova" | "Nova" => {
+                print!("Insira as informações a seguir para inserir uma nova reserva:\n");
+                println!("Insira o número da reserva: ");
+                io::stdin().read_line(&mut nm_re).expect("Insira um valor númerico válido");
+                let num_res: i32 = nm_re.trim().parse().unwrap();
+                
+                println!("Insira o nome do hotel: ");
+                stdin().read_line(&mut nm_ht).unwrap();
+                let nom_hot: String = nm_ht.trim().parse().unwrap();
 
-    stdin().read_line(&mut input2).unwrap();
-    let res2: String = input2.trim().parse().unwrap();
-    hash_map.remove(res2);
-    hash_map.display();
+                println!("Insira o número do quarto: ");
+                io::stdin().read_line(&mut nm_qt).expect("Insira um valor númerico válido");
+                let num_qua: i32 = nm_qt.trim().parse().unwrap();
+
+                println!("Insira a data do check-in: ");
+                stdin().read_line(&mut data_in).unwrap();
+                let dt_in: String = data_in.trim().parse().unwrap();
+
+                println!("Insira a data do check-out: ");
+                stdin().read_line(&mut data_ou).unwrap();
+                let dt_out: String = data_ou.trim().parse().unwrap();
+
+                let reserva: Reserva = Reserva::new(num_res, nom_hot, num_qua, dt_in, dt_out);
+                let hash: Hash = Hash::new(reserva, hash_map.values.len() as i32);
+                hash_map.push(hash);
+
+            },
+            _ => println!("Entrada não aceita!\nInsira um novo valor!")
+        }
+    }
 }
